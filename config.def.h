9e6f7b2939876f4a515a42fc6ace04f4c73ecd1c
agpsl <agnaldo.silva.mg@gmail.com>
2023-03-21 20:41:10 +0000

    Add 'Noto Color Emoji' font

--- config.def.h
+++ config.def.h
@@ -6,8 +6,10 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Regular:pixelsize=14:antialias=true:autohint=true", "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Regular:pixelsize=14:antialias=true:autohint=true",
+                                        "Noto Color Emoji:style=Regular:size=10:antialias=true:autohint=true",
+                                        "monospace:size=10", };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:style=Regular:pixelsize=14:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
